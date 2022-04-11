2050dbe8205c25460e34ae8c5faf74725bc842d6
neuro-rsu <95883191+neuro-rsu@users.noreply.github.com>
2022-04-11 18:16:05 +0300

    Add files via upload

---
+++ js/neuro-flip-card.js
@@ -0,0 +1,176 @@
+import { LitElement, html, css } from "https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js";
+
+class MyElement extends LitElement {
+    static properties = {
+        version: {},
+    };
+
+    static styles = css`
+        :host {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            height: 100%;
+            box-sizing: border-box;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        header {
+            position: absolute;
+            top: 0;
+            max-width: 100%;
+            min-width: 100%;
+            display: flex;
+            flex: 1;
+            align-items: center;
+            border-bottom: 1px solid lightgray;
+            padding: 2px;
+            z-index: 9;
+            max-height: 44px;
+            overflow: hidden;
+            overflow-x: auto;
+            box-sizing: border-box;
+        }
+        .txt {
+            border: none;
+            outline: none;
+            text-align: center;
+            font-size: 22px;
+            color: gray;
+            white-space:nowrap;
+        }
+        .board {
+            display: flex;
+            flex-direction: column;
+            align-self: center;
+            justify-content: center;
+            background-color: lightgray;
+            border: 1px solid darkgray;
+            width: 95vmin;
+            max-height: 95vmin;
+            position: relative;
+            flex: 1;
+            margin: 64px 8px 16px 8px;
+            padding: 5px;
+            overflow: hidden;
+        }
+        .row {
+            display: flex;
+            flex: 1;
+        }
+        .cell {
+            display: flex;
+            flex: 1;
+            margin: calc(1px + 1vmin/2);
+            background-color: transparent;
+            perspective: 1000px;
+            cursor: pointer;
+        }
+        .cell-inner {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 1;
+            position: relative;
+            text-align: center;
+            transition: transform 0.6s;
+            transform-style: preserve-3d;
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+            border: 1px solid darkgray;
+        }
+        .selected .cell-inner {
+            transform: rotateY(180deg);
+        }
+        .cell-front, .cell-back {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            -webkit-backface-visibility: hidden;
+            backface-visibility: hidden;
+            font-weight: 500;
+        }
+        .cell-back {
+            background-color: #bbb;
+            color: black;
+        }
+        .cell-front {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 1;
+            background-color: white;
+            transform: rotateY(180deg);
+        }
+        .odd {
+            color: transparent;
+            font-size: 0;
+            opacity: 1;
+            background-size: cover;
+            background-repeat: no-repeat;
+            cursor: default;
+        }
+    `;
+
+    constructor() {
+        super();
+        this.version = "1.0.0";
+    }
+
+    render() {
+        return html`
+        <style>
+                .solved { opacity: ${this.end ? 1 : .3}; }
+                .cell:hover .cell-inner { transform: ${this.babyMode ? 'rotateY(180deg)' : ''}; }
+                .cell-front, .cell-back { font-size: ${14 + this.fontSize - 100 <= 14 ? 14 : 14 + this.fontSize - 100}px; }
+            </style>
+            <header>
+                <li-button name='remove' border='none' size=28 @click=${() => --this.row}></li-button><div class='txt'>${this.row}</div><li-button name='add' border='none' size=28  @click=${() => ++this.row}></li-button>
+                <li-button name='remove' border='none' size=28 @click=${() => --this.column} style='margin-left: 4px'></li-button><div class='txt'>${this.column}</div><li-button name='add' border='none' size=28  @click=${() => ++this.column}></li-button>
+                <div style="display: flex; flex-direction: column; flex: 1; width: 100%">
+                    <div class='txt' style="width: 100%; ">flips - ${this.mode}</div>
+                    <div style="display: flex; width: 100%; justify-content: center; align-items: center">
+                        <div style="color: green; flex: 1; text-align: right; font-weight: 600; opacity: .5">${this.isOk}</div>
+                        <div style="padding: 0 4px"> : </div>
+                        <div style="color: red; flex: 1; font-weight: 600; opacity: .5">${this.isError}</div>
+                    </div>
+                </div>
+                <li-button name='face' border='none' size=28 @click=${() => this.babyMode = !this.babyMode} title='baby mode' toggledClass='ontoggled' ?toggled=${this.babyMode} style='margin: 0 8px 0 36px'></li-button>
+                <li-button name='extension' border='none' size=28 @click=${this.setMode} title='mode' style='margin-right: 8px'></li-button>
+                <li-button name='refresh' border='none' size=28 @click=${() => document.location.reload()} title='refresh' style='margin-right: 8px'></li-button>
+            </header>
+            <div id="board" class='board'>
+                ${[...Array(+this.row).keys()].map(row => html`
+                    <div class='row'>
+                        ${[...Array(+this.column).keys()].map(column => {
+                            let idx = this.column * row + column;
+                            return html`
+                                <div class='cell ${(this.solved.includes(idx) || idx === this.card1?.id || idx === this.card2?.id) ? 'selected' : ''} ${this.solved.includes(idx) ? 'solved' : ''}'
+                                         @click=${e => this.onclick(e, idx, this.cards?.[idx])}>
+                                    <div class='cell-inner'>
+                                        <div class='cell-front ${idx === this.odd ? 'odd' : ''}' style="color: hsla(${this.cards?.[idx]?.c || 0}, 60%, 50%, 1);">
+                                            ${this.mode === 'images' || this.mode === 'colors' || idx === this.odd ? html`
+                                                <img src=${this.cards?.[idx]?.v || this._url + 'li.png'} style="width: 100%;max-height: 100%;">
+                                            ` : html`
+                                                ${this.cards?.[idx]?.v}
+                                            `}
+                                        </div>
+                                        <div class='cell-back ${idx === this.odd ? 'odd' : ''}'>
+                                            ${idx === this.odd ? html`
+                                                <img src=${this._url + 'li.png'} style="width: 100%;max-height: 100%;">
+                                            ` : html``}
+                                        </div>
+                                    </div>
+                                </div>
+                            `})}
+                    </div>
+                `)}
+            </div>
+
+        `;
+    }
+}
+customElements.define("my-element", MyElement);
